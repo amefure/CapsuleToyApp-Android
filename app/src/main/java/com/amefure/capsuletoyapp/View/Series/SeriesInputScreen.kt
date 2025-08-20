71688f2db01e77e9702d2c6c5f7755ee136469d9
amefure <shachi10969@gmail.com>
2025-08-20 16:11:04 +0900

    入力要素の追加

--- app/src/main/java/com/amefure/capsuletoyapp/View/Series/SeriesInputScreen.kt
+++ app/src/main/java/com/amefure/capsuletoyapp/View/Series/SeriesInputScreen.kt
@@ -3,9 +3,10 @@ package com.amefure.capsuletoyapp.View.Series
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
@@ -15,6 +16,7 @@ import androidx.navigation.NavHostController
 import com.amefure.capsuletoyapp.ViewModel.SeriesViewModel
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.text.input.KeyboardType
 
 @Composable
 fun SeriesInputScreen(
@@ -23,8 +25,8 @@ fun SeriesInputScreen(
     viewModel: SeriesViewModel = hiltViewModel(),
 ) {
     var name by rememberSaveable { mutableStateOf("") }
-    var count by rememberSaveable { mutableStateOf(0) }
-    var amount by rememberSaveable { mutableStateOf(0) }
+    var count: Int? by rememberSaveable { mutableStateOf(null) }
+    var amount: Int? by rememberSaveable { mutableStateOf(null) }
     var memo by rememberSaveable { mutableStateOf("") }
 
     Column {
@@ -33,7 +35,7 @@ fun SeriesInputScreen(
                 onClick = {
                     viewModel.addSeries(
                         name = name,
-                        count = count,
+                        count = count ?: 0,
                         amount = 100,
                         memo = "メモ",
                         capsuleToys = emptyList(),
@@ -47,37 +49,65 @@ fun SeriesInputScreen(
             }
         }
 
-        TextField(
+        Row(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Column {
+                OutlinedTextField(
+                    value = if (amount == null) "" else amount.toString(),
+                    onValueChange = {
+                        if (it.isEmpty()) {
+                            amount = null
+                        } else {
+                            amount = it.toIntOrNull() ?: 0
+                        }
+                    },
+                    label = { Text("金額") },
+                    placeholder = { Text("例：300円") },
+                    keyboardOptions = KeyboardOptions.Default.copy(
+                        keyboardType = KeyboardType.Number
+                    ),
+                    singleLine = true,
+                )
+
+                OutlinedTextField(
+                    value = if (count == null) "" else count.toString(),
+                    onValueChange = {
+                        if (it.isEmpty()) {
+                            count = null
+                        } else {
+                            count = it.toIntOrNull() ?: 0
+                        }
+                    },
+                    label = { Text("種類") },
+                    placeholder = { Text("例：6種類") },
+                    keyboardOptions = KeyboardOptions.Default.copy(
+                        keyboardType = KeyboardType.Number
+                    ),
+                    singleLine = true,
+                )
+            }
+        }
+
+        OutlinedTextField(
             value = name,
             onValueChange = {
                 name = it
             },
+            label = { Text("シリーズ名") },
+            singleLine = true,
             modifier = Modifier.fillMaxWidth()
         )
 
-
-        TextField(
-            value = count.toString(),
-            onValueChange = {
-                count = it.toIntOrNull() ?: 0
-            },
-            modifier = Modifier.fillMaxWidth()
-        )
-        TextField(
-            value = amount.toString(),
-            onValueChange = {
-                amount = it.toIntOrNull() ?: 0
-            },
-            modifier = Modifier.fillMaxWidth()
-        )
-
-        TextField(
+        OutlinedTextField(
             value = memo,
             onValueChange = {
                 memo = it
             },
             modifier = Modifier.fillMaxWidth()
         )
+
+        Text("※ ガチャガチャのアイテムはシリーズを登録した後に、一覧からそのシリーズをタップすることで追加できます。")
 
     }
 }
