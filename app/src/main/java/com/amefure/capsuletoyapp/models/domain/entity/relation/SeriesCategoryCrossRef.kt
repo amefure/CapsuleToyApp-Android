2899e46e209710039e9b085918c666aa9acba79b
amefure <shachi10969@gmail.com>
2025-09-12 22:48:58 +0900

    ./gradlew spotlessApplyの実行

--- app/src/main/java/com/amefure/capsuletoyapp/models/domain/entity/relation/SeriesCategoryCrossRef.kt
+++ app/src/main/java/com/amefure/capsuletoyapp/models/domain/entity/relation/SeriesCategoryCrossRef.kt
@@ -12,7 +12,7 @@ import com.amefure.capsuletoyapp.models.domain.entity.Series
     // 警告解消：The column categoryId in the junction entity ... is being used to resolve a relationship but it is not covered by any index.
     // 「リレーションを解決するときに全件スキャンになる可能性がある」と警告
     // 該当カラムに @Index を付けてインデックスを作成
-    indices = [Index(value = [Category.ID_KEY])]
+    indices = [Index(value = [Category.ID_KEY])],
 )
 data class SeriesCategoryCrossRef(
     val seriesId: Long,
